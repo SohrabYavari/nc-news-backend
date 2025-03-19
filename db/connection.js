6adf573bcb9d365f375fd01df455c27e4ee22610
SohrabYavari <sohrabyavari00@gmail.com>
2025-03-19 14:40:33 +0000

    hosting changes 2

--- db/connection.js
+++ db/connection.js
@@ -5,13 +5,15 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const db = new Pool({});
 
+const config = {}
+
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
